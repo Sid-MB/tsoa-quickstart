39b85e576dab891099fb6006b2b953ee81bf3504
Siddharth <64648985+Sid-MB@users.noreply.github.com>
2024-05-11 14:19:55 -0700

    Live reload and Swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { json, urlencoded } from "express";
+import type { Request as ExpressRequest, Response as ExpressResponse } from "express";
+import swaggerUi from "swagger-ui-express";
 import { RegisterRoutes } from "../build/routes";
 
 export const app = express();
@@ -10,5 +12,10 @@ app.use(
 	})
 );
 app.use(json());
+
+// Expose a /docs endpoint for API documentation using Swagger UI
+app.use("/docs", swaggerUi.serve, async (_req: ExpressRequest, res: ExpressResponse) => {
+	return res.send(swaggerUi.generateHTML(await import("../build/swagger.json")));
+});
 
 RegisterRoutes(app);
